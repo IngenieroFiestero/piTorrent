5f9a05787820a2f28aac12180989581a43d56d47
Samuel <ingenierofiestero@gmail.com>
2015-02-19 04:33:35 +0100

    Version estable

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var WebTorrent = require('webtorrent');
 var fs = require('fs');
 var http = require('http');
+var Torrent = require('./node_modules/webtorrent/lib/torrent');
 
 var app = express();
 var client = new WebTorrent();
@@ -14,19 +15,47 @@ var client = new WebTorrent();
 var port= 3000;
 //Configurar torrent cliente
 
+function showTorrentList(){
+  console.log("Lista de torrents: ");
+  client.torrents.forEach(function(val,i){
+    console.log(client.torrents[i].name + " -- Descargado: " + (100*client.torrents[i].downloaded / client.torrents[i].parsedTorrent.length).toFixed(1) +" %");
+  });
+  console.log("----------");
+}
+function comprobeHash(hash){
+  client.torrents.forEach(function(val,i){
+    if(client.torrents[i].infoHash == hash)
+    console.log(client.torrents[i].infoHash);
+  });
+}
+
 function onTorrent(torrent){
+  torrent.porcentaje = 0;
   console.log("Descargando un nuevo torrent: " + torrent.name);
+  console.log("----------");
+  showTorrentList();
   torrent.files.forEach(function (file) {
-    // Stream each file to the disk
     var source = file.createReadStream();
     fs.mkdir("./files/"+ torrent.name, function(){
       var destination = fs.createWriteStream("./files/"+ torrent.name + "/" + file.name);
       source.pipe(destination);
     });
   });
-  console.log("Progreso: empezando...");
-  torrent.on('download', function(){
-    console.log("Descargado");
+  torrent.swarm.on('download', function(){
+    var progress = (100 * torrent.downloaded / torrent.parsedTorrent.length).toFixed(1);
+    if(progress >= 100 && torrent.porcentaje < 100){
+      showTorrentList();
+      torrent.porcentaje = 100;
+    }else if(progress >=25 && torrent.porcentaje < 25){
+      showTorrentList();
+      torrent.porcentaje = 25;
+    }else if(progress >=50 && torrent.porcentaje < 50){
+      showTorrentList();
+      torrent.porcentaje = 50;
+    }else if(progress >=75 && torrent.porcentaje < 75){
+      showTorrentList();
+      torrent.porcentaje = 75;
+    };
   });
 };
 
@@ -49,7 +78,7 @@ app.get('/', function(req, res, next) {
 app.post('/',function(req, res, next){
   if(req.body.newTorrentLink){
     client.add(req.body.newTorrentLink, onTorrent);
-  }
+  };
   res.render('index',{title:'Express'});
 });
 server = http.createServer(app);
